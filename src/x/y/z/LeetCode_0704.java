1543f7d52b675d9f797a2fbcfcd5400598d137a1
Ren-XingYu <379271608@qq.com>
2021-04-01 23:32:27 +0800

    leetcode 0704

---
+++ src/x/y/z/LeetCode_0704.java
@@ -0,0 +1,18 @@
+package x.y.z;
+
+public class LeetCode_0704 {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
